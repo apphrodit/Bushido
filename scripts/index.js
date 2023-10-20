9c695915d81ce2a51a8c7e9009c1798e72cac3de
apphrodit <joaovrdf@gmail.com>
2023-10-20 10:44:11 -0300

    Add Functions

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,8 @@
-//Função para Criar os "Cards"
-
-function criarCard() {
-
-//     const card =  document.createElement('div');
-//     card.className = 'card';
-//     document.body.appendChild(card);
-
-//     //Estilização dos cards
-
-// card.style.border = '2px solid #ff6600'
-
-const mainCard = document.querySelector('.mainCard'); // Obtém o estilo do .mainCard
-const newCard = mainCard.cloneNode(true); // Clona o elemento .mainCard
-
-// Adicione uma classe adicional (opcional) para estilização específica
-newCard.classList.add('dynamicCard');
-
-document.body.appendChild(newCard);
-}
-
-for (let i = 0; i < 3; i++){
-    criarCard();
+function toggleAnswer() {
+    const bushidoAnswer = document.getElementById('bushidoAnswer');
+    if (bushidoAnswer.style.display === 'none') {
+        bushidoAnswer.style.display = 'block';
+    } else {
+        bushidoAnswer.style.display = 'none';
+    }
 }
